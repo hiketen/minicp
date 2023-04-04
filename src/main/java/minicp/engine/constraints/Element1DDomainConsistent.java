8b9d68f72356ae93ca9adca7947fcc4985183f5a
hiketen.skd16 <hiketen.skd16@komidi.com>
2023-04-03 23:43:17 -0700

    .

--- src/main/java/minicp/engine/constraints/Element1DDomainConsistent.java
+++ src/main/java/minicp/engine/constraints/Element1DDomainConsistent.java
@@ -34,6 +34,8 @@ public class Element1DDomainConsistent extends AbstractConstraint {
     private final IntVar y;
     private final IntVar z;
 
+    private int[] yDomValues;
+    private int[] zDomValues;
     /**
      * Creates an element constraint {@code array[y] = z}
      *
@@ -50,6 +52,60 @@ public class Element1DDomainConsistent extends AbstractConstraint {
 
     @Override
     public void post() {
-         throw new NotImplementedException("Element1D");
+      y.removeBelow(0);
+      y.removeAbove(t.length - 1);
+      yDomValues = new int[y.size()];
+      zDomValues = new int[z.size()];
+      y.propagateOnDomainChange(this);
+      z.propagateOnDomainChange(this);
+      propagate();
+    }
+
+
+    @Override
+    public void propagate() {
+
+      int nYVal, nZVal;
+
+      nYVal = y.fillArray(yDomValues);
+      for (int i = 0; i < nYVal; i++)
+        if (!z.contains(t[yDomValues[i]]))
+          y.remove(yDomValues[i]);
+
+
+
+      //System.out.printf("z:");
+      //for (int v = z.min(); v <= z.max(); v++)
+      //  if (z.contains(v))
+      //  System.out.printf(" %d", v);
+      //System.out.printf("\n\n");
+
+
+
+      nZVal = z.fillArray(zDomValues);
+      //System.out.printf("nZVal=%d\n", nZVal);
+
+      //System.out.printf("zDomValues:");
+      //for (int i = 0; i < nZVal; i++)
+      //    System.out.printf(" %d", zDomValues[i]);
+      //System.out.printf("\n\n");
+
+      Integer [] zSup = new Integer[z.size()];
+      for (int i = 0; i < zSup.length; i++)
+        zSup[i] = 0;
+
+      nYVal = y.fillArray(yDomValues);
+
+      for (int i = 0; i < nYVal; i++)
+        for (int j = 0; j < nZVal; j++)
+          if (t[yDomValues[i]] == zDomValues[j]) {
+            zSup[j] += 1;
+            break;
+          }
+
+      for (int i = 0; i < nZVal; i++)
+        if (zSup[i]==0)
+          z.remove(zDomValues[i]);
+
     }
 }
